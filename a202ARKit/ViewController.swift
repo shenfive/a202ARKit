b61be50d1f727136085c453a072bd3bb58774aa8
Danny Shen <shenfive@gmail.com>
2022-04-10 17:03:53 +0800

    map

--- a202ARKit/ViewController.swift
+++ a202ARKit/ViewController.swift
@@ -23,10 +23,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()//SCNScene(named: "art.scnassets/ship.scn")!
+        
+        
+        sceneView.debugOptions = [.showWorldOrigin,.showFeaturePoints,.showCameras]
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        
+//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let material = SCNMaterial() //新增材質
+//        material.diffuse.contents = UIColor.red //材質內容為紅色
+//        box.materials = [material] //把 box 的貼圖材質加進去
+//        let node = SCNNode(geometry: box) //新增一個 Box
+//        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
+//        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
+//
+//
+//
+//        let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
+//        text.firstMaterial?.diffuse.contents = UIColor.blue
+//        let textNode = SCNNode(geometry: text)
+//        textNode.position = SCNVector3(0, 0.05, -0.5)
+//        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+//        sceneView.scene.rootNode.addChildNode(textNode)
+         
+        let earth = SCNSphere(radius: 0.3)
+        earth.firstMaterial?.diffuse.contents = UIImage(named: "worldmap")
+        let earthNode = SCNNode(geometry: earth)
+        
+        earthNode.position = SCNVector3(0, 0, -1)
+        sceneView.scene.rootNode.addChildNode(earthNode)
+         
+        
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +66,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
